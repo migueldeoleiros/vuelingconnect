1934113b946921dcdbbcbd06f392c67dfa3126b1
migueldeoleiros <miguel.lopez@merlinsoftware.es>
2025-05-03 10:11:30 +0200

    feat: change color and title

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -16,11 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Vueling Connect',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Vueling Connect'),
     );
   }
 }
